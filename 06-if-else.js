bf2ae69eb0942e6a171ed16cc15f3422ad4f31a2
yasmany <yasmanys88@gmail.com>
2021-02-15 11:36:59 -0300

    Estudio de la condicion If Else

---
+++ 06-if-else.js
@@ -0,0 +1,9 @@
+let attendance = 15;
+
+if (attendance < 15) {
+    console.log("Es menor a 15");
+} else if (attendance === 15) {
+    console.log("Es igual a 15");
+} else {
+    console.log("Es mayor a 15");
+}
